f2dd3e6aa35cc4a1aa2a8eb997ca7ef1f96b877f
Luis <luisantonioazevedo1@gmail.com>
2024-01-26 11:01:00 -0300

    Subindo projeto inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>JETDATE</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='node_modules/bootstrap/dist/css/bootstrap.css'>
+
+
+</head>
+
+<body>
+
+    <div class="flex-box container-box color-content">
+        <div class="content-box">
+            <div class="align-center">
+                <form action="" method="post">
+                    <div class="row">
+                        <p class="h1">Logar no sistema</p>
+                    </div>
+                    <div class="row">
+                        <input type="text" placeholder="User">
+                    </div>
+                    <div class="row">
+                        <input type="password" placeholder="Password">
+                    </div>
+                    <div class="row">
+                        <input class="input-submit" type="submit">
+                    </div>
+                    <div class="row">
+                        <div class="h3">efetuar cadastro</div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src='node_modules/jquery/dist/jquery.js'></script>
+</body>
+
+</html>
